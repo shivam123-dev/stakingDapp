6ef4b14338cff29ac6d333d6065b0e0b73dd9a1b
Ryjen1 <ryjenspark@gmail.com>
2025-09-12 13:42:06 +0100

    rainbow css added

--- src/components/ConnectWallet.tsx
+++ src/components/ConnectWallet.tsx
@@ -1,5 +1,6 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 
+
 export function ConnectWallet() {
   return (
     <div className="flex justify-end mb-8">
--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,4 +1,5 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import '@rainbow-me/rainbowkit/styles.css'
 import { sepolia } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
