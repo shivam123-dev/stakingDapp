7f247dc87badc1be9807e3ce6cac71cb2e795e41
Ryjen1 <ryjenspark@gmail.com>
2025-12-02 05:15:54 +0100

    style: add AppKit styling and custom theming

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "@reown/appkit/styles.css";
+import "./styles/appkit.css";
 import App from "./App.tsx";
 import { Providers } from "./components/Providers.tsx";
 
